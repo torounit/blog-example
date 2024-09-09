90232737910b102141dee42f48c30efc5393b008
Hiroshi Urabe <mail@torounit.com>
2024-09-09 16:16:20 +0900

    add supabase auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main>
-      Hello World
-    </main>
-  );
+import { createClient } from "@/utils/supabase/server";
+export default async function Home() {
+    const supabase = createClient();
+    const { data } = await supabase.auth.getUser();
+    console.log(data);
+    return (
+        <main>
+            Hello! {data?.user?.email}
+        </main>
+    );
 }
