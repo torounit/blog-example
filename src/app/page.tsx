640908e627f30b4ec3cfc9b9c10172a5949c8aa5
Hiroshi Urabe <mail@torounit.com>
2024-09-09 22:25:04 +0900

    fix auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
 import { createClient } from "@/utils/supabase/server";
 export default async function Home() {
     const supabase = createClient();
     const { data } = await supabase.auth.getUser();
-    console.log(data);
     return (
         <main>
             Hello! {data?.user?.email}
